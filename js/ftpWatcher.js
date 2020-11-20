85d568d1d8b248d918e972b0cddcd4df2dc4eb8d
Alberto Soler <asoler@avansis.es>
2020-11-20 12:24:54 +0100

    New functions to handle tournament variable creation

---
+++ js/ftpWatcher.js
@@ -0,0 +1,67 @@
+const { tournamentMaker } = require('./tournamentMaker')
+
+export const fileWatcher = function(sftp, folder, event) {
+    let job = function(baseObjList) {
+            folderObjList = {};
+            sftp.readdir(folder, function(err, objList) {
+                if (err) {
+                    event.emit('error', err.message || err);
+                } else {
+                    if (baseObjList === null) {
+                        objList.forEach(function(fileObj) {
+                            folderObjList[fileObj.filename] = fileObj;
+                        });
+                    } else {
+                        objList.forEach(function(fileObj) {
+                            if (!baseObjList[fileObj.filename] || (baseObjList[fileObj.filename] && fileObj.attrs.size != baseObjList[fileObj.filename].attrs.size)) {
+                                fileObj.status = "uploading";
+                            } else if (baseObjList[fileObj.filename].status == "uploading") {
+                                if (fileObj.attrs.size == baseObjList[fileObj.filename].attrs.size) {
+                                    if (baseObjList[fileObj.filename].filename === 'games.pgn') {                            
+                                        let sourceFile = sftp.createReadStream(config.path + '/' + fileObj.filename)
+                                        sourceFile.on('data', (data) => {
+                                                                 
+                                            // Split games into an array
+                                            let games = data.toString().split('[Event')
+                                            splitGames = games.map(game => '[Event' + game)
+                                            splitGames.shift()
+                                            
+                                            // Create a tournament array with all the games
+                                            const tournament = tournamentMaker(splitGames)                                             
+                                                    
+                                            delete fileObj.status;
+                                            event.emit("upload", {                                
+                                                tournament: tournament
+                                            });
+                                        })
+                                    }
+
+                                }
+                            }
+                            folderObjList[fileObj.filename] = fileObj;
+                        });
+
+
+                    }
+                    if (baseObjList && Object.keys(baseObjList).length != 0) {
+                        Object.keys(baseObjList).forEach(function(filename) {
+                            if (!folderObjList[filename]) {
+                                event.emit("delete", {
+                                    host: config.host,
+                                    user: config.username,
+                                    folder: config.path,
+                                    file: baseObjList[filename]
+                                });
+                            }
+                        });
+                    }
+                }
+            });
+        },
+        folderObjList = null;
+    timeinterval = setInterval(function() {
+        new job(JSON.parse(JSON.stringify(folderObjList)));
+        event.emit('heartbeat', new Date());
+    }, 500);
+
+};
---
+++ js/tournamentMaker.js
@@ -0,0 +1,40 @@
+const { Chess } = require('chess.js')
+
+export const tournamentMaker = (games) => {
+
+    let tournament = []
+    
+    games.forEach(game => {
+
+        let chess1 = new Chess
+        let chess2 = new Chess
+        let chess3 = new Chess 
+
+        let fens = []
+        let pgns = []
+
+        let pgn = game                 
+
+        chess1.load_pgn(pgn)
+
+        fens = chess1.history().map(move => {                                
+          chess2.move(move)                                    
+          return chess2.fen()
+        })
+
+        pgns = chess1.history().map(move => {
+          chess3.move(move)
+          return chess3.pgn()
+        })
+        
+        tournament.push({
+            headers: chess1.header(),
+            pgns: pgns,
+            match: fens
+        })
+
+    })
+    
+    return tournament
+
+}
